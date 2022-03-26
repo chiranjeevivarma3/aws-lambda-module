f594ac5d0fad9c1cb2da0f891c08c09492bac7bb
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 21:50:22 +0200

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "function_name" {
+  description = "A unique name for your Lambda Function"
+  type        = string
+  default     = ""
+}
+
+variable "handler" {
+  description = "Lambda Function entrypoint in your code"
+  type        = string
+  default     = ""
+}
+
+variable "runtime" {
+  description = "Lambda Function runtime"
+  type        = string
+  default     = ""
+}
