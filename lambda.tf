d5a6c4994d61b0b52ea98eb8d6967ea10cdb1896
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 22:00:59 +0200

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -9,10 +9,10 @@ data "archive_file" "zip_the_python_code" {
 
 resource "aws_lambda_function" "lambda_funct" {
   filename         = local.lambda_code_location
-  function_name    = "test"
+  function_name    = "var.fuction_name"
   role             = aws_iam_role.iam_role_for_lambda.arn
-  handler          = "testmodule.hello"
-  runtime          = "python3.8"
+  handler          = "var.handler"
+  runtime          = "var.runtime"
   depends_on       = [aws_iam_role_policy_attachment.logs]
   source_code_hash = filebase64sha256("testmodule.py")
 }
