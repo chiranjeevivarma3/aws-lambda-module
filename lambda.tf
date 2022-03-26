3e74387a98a7e2483da42791612071dea28dd6cd
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 21:49:48 +0200

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,18 @@
+locals {
+  lambda_code_location = "outputs/testmodule.zip"
+}
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_file = "testmodule.py"
+  output_path = local.lambda_code_location
+}
+
+resource "aws_lambda_function" "lambda_funct" {
+  filename         = local.lambda_code_location
+  function_name    = "test"
+  role             = aws_iam_role.iam_role_for_lambda.arn
+  handler          = "testmodule.hello"
+  runtime          = "python3.8"
+  depends_on       = [aws_iam_role_policy_attachment.logs]
+  source_code_hash = filebase64sha256("testmodule.py")
+}
