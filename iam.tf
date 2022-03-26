ca08681d725db2185a8ffe6457bba2f402442a32
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 21:55:26 +0200

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "iam_role_for_lambda" {
   name               = "iam_role_for_lambda"
-  assume_role_policy = file("iam/iam-role-lambda.json")
+  assume_role_policy = file("iam-role-lambda.json")
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
   name   = "iam_policy_for_lambda"
-  policy = file("iam/iam-policy-lambda.json")
+  policy = file("iam-policy-lambda.json")
 }
 
 resource "aws_iam_role_policy_attachment" "logs" {
