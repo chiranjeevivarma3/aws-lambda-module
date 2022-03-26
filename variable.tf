38a37b2e0c8b25140c9a74d912ca664167aefa5a
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 22:44:03 +0200

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,17 @@
 variable "function_name" {
   description = "A unique name for your Lambda Function"
   type        = string
-  default     = ""
+
 }
 
 variable "handler" {
   description = "Lambda Function entrypoint in your code"
   type        = string
-  default     = ""
 }
 
 variable "runtime" {
   description = "Lambda Function runtime"
   type        = string
-  default     = ""
 }
 variable "source_path" {
   description = "The absolute path to a local file or directory containing your Lambda source code"
