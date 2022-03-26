8295cf150831a59158b16a1516c411bbb2981fd0
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 22:47:34 +0200

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -9,7 +9,7 @@ data "archive_file" "zip_the_python_code" {
 
 resource "aws_lambda_function" "lambda_funct" {
   filename         = local.lambda_code_location
-  function_name    = var.fuction_name
+  function_name    = var.function_name
   role             = aws_iam_role.iam_role_for_lambda.arn
   handler          = var.handler
   runtime          = var.runtime
