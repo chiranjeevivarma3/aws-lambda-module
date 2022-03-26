07d98d263205b63923514a555c2f44bce418c024
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 22:14:17 +0200

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "iam_role_for_lambda" {
+  name               = "iam_role_for_lambda"
+  assume_role_policy = file("iam-role-lambda.json")
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name   = "iam_policy_for_lambda"
+  policy = file("iam-policy-lambda.json")
+}
+
+resource "aws_iam_role_policy_attachment" "logs" {
+  role       = aws_iam_role.iam_role_for_lambda.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
