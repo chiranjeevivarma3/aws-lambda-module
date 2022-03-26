7a5d3d6857f8a0f3ac434a612d344563fff782e4
chiranjeevivarma3 <45592634+chiranjeevivarma3@users.noreply.github.com>
2022-03-26 22:22:47 +0200

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,11 +1,38 @@
 resource "aws_iam_role" "iam_role_for_lambda" {
   name               = "iam_role_for_lambda"
-  assume_role_policy = file("${path.module}/iam-role-lambda.json")
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+       {
+         "Action": "sts:AssumeRole",
+         "Principal": {
+           "Service": "lambda.amazonaws.com"
+         },
+         "Effect": "Allow",
+         "Sid": ""
+       }
+    ]
+}
+EOF
+  
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
   name   = "iam_policy_for_lambda"
-  policy = file("${path.module}/iam-policy-lambda.json")
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1648319000945",
+      "Action": "cloudwatch:*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "logs" {
